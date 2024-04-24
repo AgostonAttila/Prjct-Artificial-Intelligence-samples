f583772136b8868577d947f78f4469de763509e9
Atesz014 <atesz014@gmail.com>
2024-04-24 23:45:44 +0200

    finish terapist

--- OpenAI - AI Therapist/text2speech.py
+++ OpenAI - AI Therapist/text2speech.py
@@ -1,1 +1,38 @@
 
+import os
+from dotenv import load_dotenv
+
+from deepgram import (
+    DeepgramClient,
+    SpeakOptions,
+)
+
+load_dotenv()
+
+SPEAK_OPTIONS = {"text": "Hello, how can I help you today?"}
+filename = "output.wav"
+
+
+def text2speech(text):
+    try:
+        SPEAK_OPTIONS = {"text" : text}
+        # STEP 1: Create a Deepgram client using the API key from environment variables
+        deepgram = DeepgramClient(api_key=os.getenv("DG_API_KEY"))
+
+        # STEP 2: Configure the options (such as model choice, audio configuration, etc.)
+        options = SpeakOptions(
+            model="aura-asteria-en",
+            encoding="linear16",
+            container="wav"
+        )
+
+        # STEP 3: Call the save method on the speak property
+        response = deepgram.speak.v("1").save(filename, SPEAK_OPTIONS, options)
+        print(response.to_json(indent=4))
+
+    except Exception as e:
+        print(f"Exception: {e}")
+
+
+if __name__ == "__main__":
+    main()
